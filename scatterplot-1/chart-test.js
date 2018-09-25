5991d8bc7a0d0ddcd9a8c968f2570ea2950bdf54
zachalexander <alexander.d.zachary@gmail.com>
2018-09-25 13:12:02 -0400

    plays around with scatterplot values

--- scatterplot-1/chart-test.js
+++ scatterplot-1/chart-test.js
@@ -1,12 +1,51 @@
-var data = [4, 8, 15, 16, 23, 42];
+var dataset = [
+ [ 5, 20 ],
+ [ 480, 90 ],
+ [ 250, 50 ],
+ [ 100, 33 ],
+ [ 330, 95 ],
+ [ 410, 12 ],
+ [ 475, 44 ],
+ [ 25, 67 ],
+ [ 85, 21 ],
+ [ 220, 88 ]
+ ];
 
-var x = d3.scaleLinear()
-    .domain([0, d3.max(data)])
-    .range([0, 420]);
+let w = 500;
+let h = 100;
 
-d3.select(".chart")
-  .selectAll("div")
-    .data(data)
-  .enter().append("div")
-    .style("width", function(d) { return x(d) + "px"; })
-    .text(function(d) { return d; });
+var svg = d3.select("body")
+ .append("svg")
+ .attr("width", w)
+ .attr("height", h);
+
+ svg.selectAll("circle")
+    .data(dataset)
+    .enter()
+    .append("circle")
+    .attr("cx", function(d){
+      return d[0];
+    })
+    .attr("cy", function(d){
+      return d[1];
+    })
+    .attr("r", function(d){
+      return Math.sqrt(h - d[1]);
+    });
+
+  svg.selectAll("text")
+     .data(dataset)
+     .enter()
+     .append("text")
+     .text(function(d){
+       return d[0] + "," + d[1];
+     })
+     .attr("x", function(d){
+       return d[0];
+     })
+     .attr("y", function(d){
+       return d[1];
+     })
+     .attr("font-family", "sans-serif")
+     .attr("font-size", "11px")
+     .attr("fill", "red");
